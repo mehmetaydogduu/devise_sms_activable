33b7af1b038a366e1fd2c75e9bfdaff88f52d8dc
Mehmet Aydoğdu <140202062@kocaeli.edu.tr>
2018-04-27 17:18:26 +0300

    ActiveRecord::Current for migration

--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,4 +1,4 @@
-class DeviseSmsActivableAddTo<%= table_name.camelize %> < ActiveRecord::Migration
+class DeviseSmsActivableAddTo<%= table_name.camelize %> < ActiveRecord::Migration::Current
   def self.up
     change_table :<%= table_name %> do |t|
       t.string   :phone
