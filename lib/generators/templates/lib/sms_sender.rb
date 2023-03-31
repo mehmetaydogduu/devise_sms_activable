a7c1b95496e054f1e1fec8be1fc33e5ad9ed3699
Mehmet Aydoğdu <memet@akillibulut.net>
2023-03-31 00:11:06 +0000

    update

--- lib/generators/templates/lib/sms_sender.rb
+++ lib/generators/templates/lib/sms_sender.rb
@@ -1,7 +1,5 @@
 class Devise::SmsSender
-  #Actually sends the sms token. feel free to modify and adapt to your provider and/or gem
+  # Actually not sends the sms token. feel free to modify and adapt to your provider and/or gem
   def self.send_sms(phone,message)
-    sms = Moonshado::Sms.new(phone, message)
-    return sms.deliver_sms[:stat]
   end
 end
